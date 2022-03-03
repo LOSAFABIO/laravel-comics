95551e753dab56df5a2fcf3a3f2653e3a2bc6a78
LOSAFABIO <fabio.losa95@gmail.com>
2022-03-03 19:06:23 +0100

    problema con scss, va in loop watch

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -15,8 +15,3 @@
         </div>
     </div>
 @endsection
-
-
-{{-- ok struttura laravel
-ho un problema in terminale quando vato a digitare "npm run watch", ogni volta che lo scrivo esce questo errore "code: 'ERR_OSSL_EVP_UNSUPPORTED'"
-ho aggiornato node (suggerito quando mi usciva errore), ma rimane comunque il problema, ho anche eliminato node_modules e reinstallato e scritto "npm run dev" ma non cambia nulla! --}}
